3936036e1e02f7197782355c895b0c41d5e47523
lcy-three <lichunyan1993@foxmail.com>
2019-08-31 10:59:21 +0800

    测试项目提交

---
+++ src/Test03.java
@@ -0,0 +1,12 @@
+/**
+ * ClassName: Test03
+ * Function:  TODO
+ * Date:      2019/8/29 22:14
+ * author     lcy
+ * version    V1.0
+ */
+public class Test03 {
+    public static void main(String[] args) {
+        System.out.println("hahahahaha");
+    }
+}
